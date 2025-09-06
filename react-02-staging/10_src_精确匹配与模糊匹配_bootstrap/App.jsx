1673a5341679317fb67668ae9b90c7561a21d61f
emon <liming20110711@163.com>
2025-09-06 08:07:35 +0800

    update:20250906-02

--- react-02-staging/10_src_精确匹配与模糊匹配_bootstrap/App.jsx
+++ react-02-staging/10_src_精确匹配与模糊匹配_bootstrap/App.jsx
@@ -11,8 +11,8 @@ import "./App.css";
 // 创建并暴露App组件
 export default class App extends Component {
     /*
-    路由的严格匹配于模糊匹配
-    1.默认使用的事模糊匹配（简单记：【输入的路径】必须包含要【匹配的路径】，且顺序要一致）
+    路由的严格匹配与模糊匹配
+    1.默认使用的是模糊匹配（简单记：【输入的路径】必须包含要【匹配的路径】，且顺序要一致）
     2.开启严格匹配：<Route exact={true} path="/home" component={Home}/>
     3.严格匹配不要随便开启，需要再开，有些时候开启会导致无法继续匹配二级路由
      */
@@ -31,7 +31,7 @@ export default class App extends Component {
                             {/*<a className="list-group-item" href="./about.html">About</a>
                             <a className="list-group-item active" href="./home.html">Home</a>*/}
 
-                            {/*在Reeact中靠路由链接实现切换组件——编写路由链接*/}
+                            {/*在React中靠路由链接实现切换组件——编写路由链接*/}
                             <MyNavLink to="/about">About</MyNavLink>
                             <MyNavLink to="/home/a/b">Home</MyNavLink>
                         </div>
